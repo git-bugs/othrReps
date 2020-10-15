9617ef5d62ef4af5bfa998d2118dde418610a39e
git-bugs <voov3r@gmail.com>
2020-10-15 20:02:51 +0300

    game-hard

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
 
 
 let changeNum = function(){
-  let y = 55;
+  let y = Math.ceil(Math.random()*100),
+      n = 10;
   function game(){
-    let x = prompt('Угадай число от 1 до 100');
-    if (x > y) {
-      alert('Загаданное число меньше');
-      game();
-    } else if ( x < y && x !== null) {
-      alert('Загаданное число больше');
-      game();
-    } else if ( x == y) {
-      alert('Поздравляю, Вы угадали!!!');
-    } else if (isNaN(x)) {
+    let x = prompt(`Угадай число от 1 до 100. Попыток ${n}`);
+    n--;
+    if ( n === 0) {
+      let end = confirm('Попытки закончились, хотите сыграть еще?');
+      if (end){
+        changeNum();
+      } else {
+      alert('Игра окончена');
+      }
+    } else if (x === null) {
+      alert('Игра окончена');
+    } else if (isNaN(x) || x.trim() === '') {
+      n++;
       alert('Введи число!');
       game();
-    } else if (x===null) {
-      alert('Игра окончена')
-    }
+    } else if (x > y) {
+      alert(`Загаданное число меньше, осталось попыток ... ${n} `);
+      game();
+    } else if ( x < y ) {
+      alert(`Загаданное число больше, осталось попыток ... ${n} `);
+      game();
+    } else if ( x == y) {
+      let win = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+      if (win) {
+        changeNum();
+      } else {
+        alert('Игра окончена');
+      }
+    }  
   };
   game();
 };
